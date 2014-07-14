f85b332b3724c1a203501b2f23d37d803cf9b7ec
Jing Wang <jwang@istuary.com>
2014-07-13 23:48:43 -0700

    added disabled option for checklist

--- dist/js/xeditable.js
+++ dist/js/xeditable.js
@@ -1,7 +1,7 @@
 /*!
 angular-xeditable - 0.1.8
 Edit-in-place for angular.js
-Build date: 2014-06-05 
+Build date: 2014-07-13 
 */
 /**
  * Angular-xeditable module 
@@ -137,12 +137,14 @@ angular.module('xeditable').directive('editableChecklist', [
       render: function() {
         this.parent.render.call(this);
         var parsed = editableNgOptionsParser(this.attrs.eNgOptions);
-        var html = '<label ng-repeat="'+parsed.ngRepeat+'">'+
-          '<input type="checkbox" checklist-model="$parent.$data" checklist-value="'+parsed.locals.valueFn+'">'+
-          '<span ng-bind="'+parsed.locals.displayFn+'"></span></label>';
+          var html = '<label ng-repeat="'+parsed.ngRepeat+'">'+
+              '<input type="checkbox" ng-disabled="' + this.attrs.eNgDisabled + '" ng-change="' + this.attrs.eNgChange + '" checklist-model="$parent.$data" checklist-value="'+parsed.locals.valueFn+'">'+
+              '<span ng-bind="'+parsed.locals.displayFn+'"></span></label>';
 
         this.inputEl.removeAttr('ng-model');
         this.inputEl.removeAttr('ng-options');
+        this.inputEl.removeAttr('ng-change');
+        this.inputEl.removeAttr('ng-disabled');
         this.inputEl.html(html);
       }
     });
--- src/js/directives/checklist.js
+++ src/js/directives/checklist.js
@@ -10,12 +10,14 @@ angular.module('xeditable').directive('editableChecklist', [
       render: function() {
         this.parent.render.call(this);
         var parsed = editableNgOptionsParser(this.attrs.eNgOptions);
-        var html = '<label ng-repeat="'+parsed.ngRepeat+'">'+
-          '<input type="checkbox" checklist-model="$parent.$data" checklist-value="'+parsed.locals.valueFn+'">'+
-          '<span ng-bind="'+parsed.locals.displayFn+'"></span></label>';
+          var html = '<label ng-repeat="'+parsed.ngRepeat+'">'+
+              '<input type="checkbox" ng-disabled="' + this.attrs.eNgDisabled + '" ng-change="' + this.attrs.eNgChange + '" checklist-model="$parent.$data" checklist-value="'+parsed.locals.valueFn+'">'+
+              '<span ng-bind="'+parsed.locals.displayFn+'"></span></label>';
 
         this.inputEl.removeAttr('ng-model');
         this.inputEl.removeAttr('ng-options');
+        this.inputEl.removeAttr('ng-change');
+        this.inputEl.removeAttr('ng-disabled');
         this.inputEl.html(html);
       }
     });
